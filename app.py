84dcf88730f66fa39e112d3f6a815a71d2431f3c
Suraj Srivastav <118558931+surajsri23@users.noreply.github.com>
2025-04-04 12:04:45 +0530

    initial commit

--- app.py
+++ app.py
@@ -223,5 +223,4 @@ def realtime_alerts():
 
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get("PORT", 5000))  # Render provides PORT dynamically
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
